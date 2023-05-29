52d53dcabeef081710a7167e8b47f56b50a63fc2
Ali Houssain Sareini <alihsareini@live.com>
2023-05-29 09:12:31 -0400

    implemented TaskList and TaskItem component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,1 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<h1 class="text-3xl font-bold underline text-red-400">Hello world!</h1>
